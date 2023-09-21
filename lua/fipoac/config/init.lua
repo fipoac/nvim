e60bdfb83ad130df0204d9e1320246d0aceef4b1
Nico Fischer <NF@recosnet.int>
2023-09-21 09:31:17 +0200

    added color and remove treesitter

--- lua/fipoac/config/init.lua
+++ lua/fipoac/config/init.lua
@@ -3,4 +3,7 @@
 
 -- TODO in future step scan plugins directory
 require('fipoac.config.plugins.telescope')
-require('fipoac.config.plugins.treesitter')
+require('fipoac.config.plugins.rose-pine')
+
+-- set color scheme
+require('fipoac.config.colors')
